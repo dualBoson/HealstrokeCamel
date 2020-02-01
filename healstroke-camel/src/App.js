2c9d4b210940dbf7563c0cca3413ad494110247b
Pedro Canhoto <0mw1zeus@gmail.com>
2020-02-01 00:05:13 +0000

    Skeleton

--- healstroke-camel/src/App.js
+++ healstroke-camel/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				HealStrokeCamel
+			</header>
+
+			<div class="Game-Screen">
+				<div class ="HUD">
+					<div class="Close-Button">
+					</div>
+					<div class="Water-Info">
+					</div>
+					<div class="Organ-Info">
+					</div>
+					<div class="Map">
+					</div>
+					<div class="Conditions-Info">
+					</div>
+				</div>
+			</div>
+
+			<div class="Gameplay-Area">
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
